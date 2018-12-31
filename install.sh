229d179da17973597fe0dc46c0739ad1ac166211
Nitay D <>
2018-12-31 17:44:48 +0200

    Improves install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #/usr/bin/env sh
 
+set -e
+set -o pipefail
+
 DOCKER_DIR="/mnt/shared/Installs/docker-ce-18.06"
-DOCKER_DEB="docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb"
-DOCKER_LIB="libseccomp2_2.3.1-2.1ubuntu2~16.04.1_amd64.deb"
+DOCKER_DEB="./docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb"
+DOCKER_LIB="./libseccomp2_2.3.1-2.1ubuntu2~16.04.1_amd64.deb"
 
 function title
 {
@@ -12,25 +15,38 @@ function title
     echo -e $GREEN${1}$NORMAL
 }
 
-title "- add network capabilities to python..."
+# Install dependencies
+sudo apt install -y grub python
+
+# Add network capabilities to python
 sudo setcap cap_net_raw+ep $(grub-mkrelpath $(which python))
 
-title "- disable ip forwarding..."
+# Disable ip forwarding
 sudo sysctl net.ipv4.ip_forward=0
 
-title "- install docker..."
+# Install docker
 pushd $DOCKER_DIR
-sudo apt install -y $DOCKER_DEB $DOCKER_LIB
+sudo apt-get install -y $DOCKER_DEB $DOCKER_LIB
 popd
 
-title "- configure docker..."
-cat > /etc/docker/daemon.json << EOF
+sudo adduser $(whoami) docker
+sudo service docker stop
+sudo service docker start
+sleep 1
+
+# Configure docker
+sudo /bin/bash -c 'cat > /etc/docker/daemon.json << EOF
 {
     "insecure-registries" : ["jaro:1700"],
     "dns" : ["10.159.0.30", "10.159.0.50"],
     "iptables": false
 }
-EOF
-sudo adduser $(whoami) docker
+EOF'
 sudo service docker restart
+sleep 1
+
+# For this run only
+sudo chmod 777 /var/run/docker.sock
+
+# Logic to inage repository
 docker login -u masad -p masad jaro:1700
