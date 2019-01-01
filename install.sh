e442b26c9f49564fd5947b01492d557468550dd9
Yuval Moalem <>
2019-01-01 11:05:06 +0200

    Improves install.sh

--- install.sh
+++ install.sh
@@ -4,37 +4,24 @@ set -e
 set -o pipefail
 
 DOCKER_DIR="/mnt/shared/Installs/docker-ce-18.06"
-DOCKER_DEB="./docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb"
-DOCKER_LIB="./libseccomp2_2.3.1-2.1ubuntu2~16.04.1_amd64.deb"
 
-function title
-{
-    local GREEN="\033[1;32m"
-    local NORMAL="\033[0m"
+# apt packages
+DEB_DOCKER="$DOCKER_DIR/docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb"
+DEB_SECCOMP="$DOCKER_DIR/libseccomp2_2.3.1-2.1ubuntu2~16.04.1_amd64.deb"
 
-    echo -e $GREEN${1}$NORMAL
-}
+# apt installs
+sudo apt install -y \
+    $DEB_DOCKER \
+    $DEB_SECCOMP \
+    grub \
+    python
 
-# Install dependencies
-sudo apt install -y grub python
-
-# Add network capabilities to python
-sudo setcap cap_net_raw+ep $(grub-mkrelpath $(which python))
-
-# Disable ip forwarding
-sudo sysctl net.ipv4.ip_forward=0
-
-# Install docker
-pushd $DOCKER_DIR
-sudo apt-get install -y $DOCKER_DEB $DOCKER_LIB
-popd
-
+# Add user to docker group
 sudo adduser $(whoami) docker
-sudo service docker stop
-sudo service docker start
+sudo service docker restart
 sleep 1
 
-# Configure docker
+# Edit docker configuration
 sudo /bin/bash -c 'cat > /etc/docker/daemon.json << EOF
 {
     "insecure-registries" : ["jaro:1700"],
@@ -45,8 +32,14 @@ EOF'
 sudo service docker restart
 sleep 1
 
-# For this run only
+# Needed for login (for this run only)
 sudo chmod 777 /var/run/docker.sock
 
-# Logic to inage repository
+# Login to image repository
 docker login -u masad -p masad jaro:1700
+
+# Add network capabilities to python
+sudo setcap cap_net_raw+ep $(grub-mkrelpath $(which python))
+
+# Disable ip forwarding
+sudo sysctl net.ipv4.ip_forward=0
